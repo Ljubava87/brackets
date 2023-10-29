67cd780fb9f4a711e37664c6d8e005cb6641163c
unknown <you@example.com>
2023-10-29 23:39:08 +0100

    Realise brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  const stack = [];
+  const bracketsMap = new Map(bracketsConfig);
+  for (const char of str) {
+      const lastBracket = stack[stack.length - 1];
+
+      if (bracketsMap.has(char) && bracketsMap.get(lastBracket) !== char) {
+
+          stack.push(char);
+      } else if (bracketsMap.get(lastBracket) === char) {
+
+          stack.pop();
+      } else {
+          return false;
+      }
+  }
+
+  return stack.length === 0;
+};
